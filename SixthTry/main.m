99b617b1d8a70eb56a95680b249fbd8072cb4686
MissWan <alinawxn@gmail.com>
2015-06-03 16:33:00 -0400

    Initial Commit

---
+++ SixthTry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SixthTry
+//
+//  Created by Xiaonan Wang on 6/3/15.
+//  Copyright (c) 2015 Xiaonan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
